e5c8a80bd9f8e193caa65dcd8f0c1f3f8b1ae8a1
Ednux <edsonbrilhante@gmail.com>
2017-08-18 20:53:46 -0400

    Add funcao fila criar

--- fila.c
+++ fila.c
@@ -1,3 +1,12 @@
 #include "fila.h" //Inclui os protótipos
 #include <stdio.h>
 #include <stdlib.h>
+
+//Criar fila
+Fila *fila_criar(void)
+{
+	Fila *fi = (Fila*) malloc(sizeof(Fila));
+	if (fi != NULL)
+		*fi = NULL;
+	return fi;
+}
