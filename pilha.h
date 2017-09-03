4ee5a1e5320bdce3ca98296ed3e051b8e3472bb4
Ednux <edsonbrilhante@gmail.com>
2017-09-03 12:29:18 -0400

    Adicionado header da pilha

---
+++ pilha.h
@@ -0,0 +1,29 @@
+#ifndef _PILHA_H
+	//Definição da biblioteca
+	#define _PILHA_H
+	
+	//Definição dos tipos de dados
+	typedef struct no {
+		int indice;
+		struct no *prox;
+	} No;
+	
+	//Definição de uma pilha
+	typedef No *Pilha;
+	
+	//Declaração dos protótipos das funções
+		//Função para criar uma pilha
+		extern Fila *pilha_criar(void);
+		
+		//Função para liberar uma pilha
+		extern void pilha_liberar(Pilha *pi);
+		
+		//Função para inserir um nó na pilha
+		extern int pilha_inserir(Pilha *pi, No dados);
+		
+		//Função para remover um nó da pilha
+		extern int pilha_remover(Pilha *pi);
+		
+		//Função para exibir todos os nós da pilha
+		extern int pilha_exibir(Pilha *pi);
+#endif
