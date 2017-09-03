230f83f89fdb29392491cc6c2b1b0866715498eb
Ednux <edsonbrilhante@gmail.com>
2017-09-03 12:38:09 -0400

    Add funcao pilha criar

--- pilha.c
+++ pilha.c
@@ -1,3 +1,12 @@
 #include "pilha.h" //Inclui os protótipos
 #include <stdio.h>
 #include <stdlib.h>
+
+//Criar pilha
+Pilha *pilha_criar(void)
+{
+	Pilha *pi = (Pilha*) malloc(sizeof(Pilha));
+	if (pi != NULL)
+		*pi = NULL;
+	return pi;
+}
